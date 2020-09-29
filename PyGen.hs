866f0cfe948d9636f1dbaa29d63aad2acef85026
salty-Frankenstein <1328955419@qq.com>
2020-09-29 19:17:56 +0800

    more operators

--- PyGen.hs
+++ PyGen.hs
@@ -1,8 +1,63 @@
-data PyExpr = ValInt Integer
+data PyExpr = Name String
+    | ValInt Integer
+    | ValFloat Float
     | ValBool Bool
+    | ValString String
+    | PyAnd PyExpr PyExpr
+    | PyOr PyExpr PyExpr
+    | PyNot PyExpr
+    | PyGr PyExpr PyExpr
+    | PyGe PyExpr PyExpr
+    | PyLs PyExpr PyExpr
+    | PyLe PyExpr PyExpr
+    | PyEq PyExpr PyExpr
+    | PyNeq PyExpr PyExpr
+    | PyAdd PyExpr PyExpr
+    | PySub PyExpr PyExpr
+    | PyMul PyExpr PyExpr
+    | PyDiv PyExpr PyExpr
+    | PyFlrDiv PyExpr PyExpr
+    | PyMod PyExpr PyExpr
+    | PyPow PyExpr PyExpr
     deriving (Show, Eq)
 
-data PyStmt = Assign String PyExpr
+var = Name
+vi = ValInt
+vf = ValFloat
+vb = ValBool
+vs = ValString
+pynot = PyNot
+
+infixr 2 ?||
+(?||) :: PyExpr -> PyExpr -> PyExpr
+(?||) = PyOr
+
+infixr 3 ?&&
+(?&&) :: PyExpr -> PyExpr -> PyExpr
+(?&&) = PyAnd
+
+infix ?==, ?!=, ?<, ?<=, ?>, ?>=
+(?==), (?!=), (?<), (?<=), (?>), (?>=) :: PyExpr -> PyExpr -> PyExpr
+(?==) = PyEq
+(?!=) = PyNeq
+(?<) = PyLs
+(?<=) = PyLe
+(?>) = PyGr
+(?>=) = PyGe
+
+infixl 6 ?+, ?-
+(?+), (?-) :: PyExpr -> PyExpr -> PyExpr
+(?+) = PyAdd
+(?-) = PySub
+
+infixl 7 ?*, ?/, ?//, ?%
+(?*), (?/), (?//), (?%) :: PyExpr -> PyExpr -> PyExpr
+(?*) = PyMul
+(?/) = PyDiv
+(?//) = PyFlrDiv
+(?%) = PyMod
+
+data PyStmt = Assign PyExpr PyExpr
     | IfThen PyExpr PyStmt
     | IfThenElse PyExpr PyStmt PyStmt
     | Block [PyStmt]
@@ -11,14 +66,32 @@ data PyStmt = Assign String PyExpr
 data PyKey = Pyend | Pyelse PyStmt
 
 eval :: PyExpr -> String
+eval (Name n) = n
 eval (ValInt i) = show i
+eval (ValFloat f) = show f
 eval (ValBool b) = show b
+eval (ValString s) = show s
+eval (PyAdd e1 e2) = eval e1 ++ " + " ++ eval e2
+eval (PySub e1 e2) = eval e1 ++ " - " ++ eval e2
+eval (PyMul e1 e2) = eval e1 ++ " * " ++ eval e2
+eval (PyDiv e1 e2) = eval e1 ++ " / " ++ eval e2
+eval (PyFlrDiv e1 e2) = eval e1 ++ " // " ++ eval e2
+eval (PyMod e1 e2) = eval e1 ++ " % " ++ eval e2
+eval (PyAnd e1 e2) = eval e1 ++ " and " ++ eval e2
+eval (PyOr e1 e2) = eval e1 ++ " or " ++ eval e2
+eval (PyNot e) = " not " ++ eval e
+eval (PyEq e1 e2) = eval e1 ++ " == " ++ eval e2
+eval (PyNeq e1 e2) = eval e1 ++ " != " ++ eval e2
+eval (PyLs e1 e2) = eval e1 ++ " < " ++ eval e2
+eval (PyLe e1 e2) = eval e1 ++ " <= " ++ eval e2
+eval (PyGr e1 e2) = eval e1 ++ " > " ++ eval e2
+eval (PyGe e1 e2) = eval e1 ++ " >= " ++ eval e2
 
 genBlock :: PyStmt -> String
 genBlock block = concat $ map ("    "++) $ generate block
 
 generate :: PyStmt -> [String]
-generate (Assign name expr) = [name ++ "=" ++ eval expr]
+generate (Assign name expr) = [eval name ++ " = " ++ eval expr]
 generate (IfThen cond stmt) = ["if " ++ eval cond ++ ":\n", genBlock stmt]
 generate (IfThenElse cond stmt1 stmt2) =
     ["if " ++ eval cond ++ ":\n", 
@@ -27,9 +100,9 @@ generate (IfThenElse cond stmt1 stmt2) =
     genBlock stmt2]
 generate (Block l) = map ((++"\n").concat.generate) l
 
-infix 0 =:
-(=:) :: String -> PyExpr -> PyStmt
-(=:) = Assign 
+infix 0 ?=
+(?=) :: PyExpr -> PyExpr -> PyStmt
+(?=) = Assign 
 
 pyif :: PyExpr -> PyStmt -> PyKey -> PyStmt
 pyif cond stmt Pyend = IfThen cond stmt
@@ -40,16 +113,17 @@ pydo = Block
 
 main :: IO ()
 main = putStrLn.concat.generate $ 
-    let ifstmt = pyif (ValBool True) ("a" =: ValInt 2) pyend in
+    let ifstmt = pyif (var "a" ?== vi 1) (var "a" ?= vi 2) pyend in
         pydo [
-            "a" =: ValInt 1,
+            var "a" ?= vi 1,
             ifstmt,
-            pyif (ValBool False)
+            pyif (var "a" ?>= vi 0 ?&& pynot (vb True))
                 ifstmt $ 
             pyelse $
                 pydo [
-                    "a" =: ValInt 4, 
-                    "b" =: ValInt 5
+                    var "a" ?= vi 4, 
+                    var "b" ?= vf 3.14,
+                    var "c" ?= var "a" ?+ var "b" ?* vf 4.7
                 ]
         ]
 
