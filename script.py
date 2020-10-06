13238faf288e897b90aa2cb5e26078783abb6662
salty-Frankenstein <1328955419@qq.com>
2020-10-06 15:00:19 +0800

    function definition & for loop

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 def fibTail(n, a, b):
-    if n == 0:
+    if n == 1:
         return a
     return fibTail(n - 1, b, a + b)
 
 def fib(n):
-    return fibTail(n, 0, 1)
+    return fibTail(n, 1, 1)
 
 i = 1
 while i <= 10:
