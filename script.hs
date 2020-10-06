9ed4f12e0e117cfd6814e663a329bda91842c3ae
salty-Frankenstein <1328955419@qq.com>
2020-10-06 19:25:10 +0800

    extension for simpler use

--- script.hs
+++ script.hs
@@ -1,24 +1,24 @@
-import PyGen
+import PyGen.Clear
 
 defFib :: PyStmt
-defFib = pydef "fibTail" [var "n", var "a", var "b"] $ pydo [
+defFib = def "fibTail" [var "n", var "a", var "b"] $ pydo [
         pyif (var "n" ?== vI 1)
-            (pyret $ var "a")
-        pyend,
-        pyret $ pycall (var "fibTail") [var "n" ?- vI 1, var "b", var "a" ?+ var "b"]
+            (ret $ var "a")
+        endif,
+        ret $ call (var "fibTail") [var "n" ?- vI 1, var "b", var "a" ?+ var "b"]
     ]
 
 main :: IO ()
 main = writeFile "script.py" $ runScript $ 
     pydo [
         defFib,
-        pydef "fib" [var "n"] $ pyret $ pycall (var "fibTail") [var "n", vI 1, vI 1],
-        pyfor(var "i" ?= vI 1, var "i" ?<= vI 10, var "i" ?+= vI 1) $ pydo [
-            pyignore $ var "i" ?|> var "fib" ?|> var "print" -- pipelines
+        def "fib" [var "n"] $ ret $ call (var "fibTail") [var "n", vI 1, vI 1],
+        for(var "i" ?= vI 1, var "i" ?<= vI 10, var "i" ?+= vI 1) $ pydo [
+            ignore $ var "i" |> var "fib" |> var "print" -- pipelines
         ],
-        var "inc" ?= var "x" ?-> var "x" ?+ vI 1,                   -- lambda function
-        var "add" ?= var "x" ?-> var "y" ?-> var "x" ?+ var "y",    -- currying
-        pyignore $ pycall (var "print") [pycall (pycall (var "add") [vI 1]) [vI 1]], -- calling
-        pyignore $ var "print" ?<| (var "add" ?<| vI 1) ?<| vI 1 -- calling by pipelines
+        var "inc" ?= var "x" --> var "x" ?+ vI 1,                   -- lambda function
+        var "add" ?= var "x" --> var "y" --> var "x" ?+ var "y",    -- currying
+        call_ (var "print") [call (call (var "add") [vI 1]) [vI 1]], -- calling
+        ignore $ var "print" <| (var "add" <| vI 1) <| vI 1 -- calling by pipelines
     ]
 
